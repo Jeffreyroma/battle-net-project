d5c989377aca5aabff1ed8f604a0bf38f9a5079d
Jeffreyroma <romamakarcikov@gmail.com>
2023-07-13 21:02:14 +0300

    Add Base test class and  allure properties file

--- src/test/java/BattleTest.java
+++ src/test/java/BattleTest.java
@@ -5,22 +5,14 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class BattleTest {
+public class BattleTest extends BaseTest {
 
     @Test
     public void checkAuthorizationFormData() {
-        open("https://eu.battle.net/login/ru/");
-
         $(By.id("accountName")).setValue("romamakarcikov@gmail.com");
         $(By.id("password")).setValue("Bastardjeffrey123");
         $(By.id("submit")).click();
 
         $(By.xpath("//h1[@class=\"title\"]")).shouldHave(text("Обзор учетной записи"));
-
-
-
-
-
-
     }
 }
